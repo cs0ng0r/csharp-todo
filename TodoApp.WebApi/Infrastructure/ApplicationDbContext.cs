534b418ea937c8b236c1c8b65f82dd281e5a41ea
cs0ng0r <bikadcsongor@gmail.com>
2025-03-04 12:10:08 +0100

    Created:

---
+++ TodoApp.WebApi/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.WebApi.Infrastructure.Models;
+
+namespace TodoApp.WebApi.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<TodoEntry> Todos { get; set; }
+}
---
+++ TodoApp.WebApi/Infrastructure/Models/TodoEntry.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApp.WebApi.Infrastructure.Models;
+
+public class TodoEntry
+{
+    public int Id { get; set; }
+
+    [Required] public string TodoTitle { get; set; } = "";
+    public bool IsDone { get; set; }
+}
